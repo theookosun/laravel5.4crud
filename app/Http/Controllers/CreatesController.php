536626f362cb50d04576df093eeb67d80ec4faa5
Theophilus Okosun <theophilusokosun@ymail.com>
2018-03-03 22:28:03 -0500

    --edit,update commit

--- app/Http/Controllers/CreatesController.php
+++ app/Http/Controllers/CreatesController.php
@@ -25,6 +25,36 @@ class CreatesController extends Controller
               'description'=> 'required'
           ]);
 
-          return 'Validation Pass';
+         $articles= new Article;
+         $articles->title = $request->input('title');
+         $articles->description = $request->input('description');
+         $articles->save();
+         return redirect('')->with('success message', 'Article successfully Added');
+
       }
+
+      public function update($id){
+          $articles = Article::find($id);  
+          
+          return view('update',['articles'=> $articles]);
+            }
+
+            public function edit(Request $request, $id){
+                $this->validate($request, [
+                    'title'=> 'required',
+                    'description'=> 'required'
+                ]);
+                    $data = array(
+                        'title'=> $request->input('title'),
+                        'description'=> $request->input('description')
+                    );
+                    Article::where('id', $id)->update($data);
+               return redirect('/')->with('success message', 'Article updated successfully');
+            }
+
+            public function read($id){
+                $articles = Article::all();
+        
+        return view('read', ['articles' => $articles]);
+            }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/', 'CreatesController@home');
 Route::get('/create', 'CreatesController@create');
 
 Route::post('/insert', 'CreatesController@add');
+Route::get('/update/{id}', 'CreatesController@update');
+Route::post('/edit/{id}', 'CreatesController@edit');
+Route::get('/read/{id}', 'CreatesController@read');
 
 
 
